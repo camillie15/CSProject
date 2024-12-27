f30471dbf4dbc9a4045bdf2e1de46a60ebc4416f
Camillie Ayovi Villafuerte <camillieayovivillafuerte@gmail.com>
2024-12-26 22:47:34 -0500

    Update Project

--- src/main/java/com/cs/project/ProjectApplication.java
+++ src/main/java/com/cs/project/ProjectApplication.java
@@ -1,13 +1,13 @@
-package com.project.demo;
+package com.cs.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsProjectApplication {
+public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CsProjectApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
--- src/test/java/com/cs/project/ProjectApplicationTests.java
+++ src/test/java/com/cs/project/ProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.demo;
+package com.cs.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CsProjectApplicationTests {
+class ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
