65e14c3edb2d6a254dd41099f615a1b6a97912c1
ErickCor24 <erick.cordovavit@ug.edu.ec>
2025-01-02 20:41:12 -0500

    Login dont nulls & not register similar email & username

--- src/main/resources/static/js/LoginScript.js
+++ src/main/resources/static/js/LoginScript.js
@@ -1,6 +1,20 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
- */
 
 
+const formLogin = document.getElementById("login-form");
+
+
+formLogin.addEventListener('submit', (e) => {
+    const email = formLogin.elements['email'].value.trim();
+    const password = formLogin.elements['password'].value.trim();
+    if (testNull(email, password)) {
+        e.preventDefault(); // Detiene el envío del formulario
+        console.log("Campos vacíos");
+        return; // Salir de la función si hay campos vacíos
+    }
+});
+
+
+// Retorna true si algún campo del form está vacío
+function testNull(...fields) {
+    return fields.some(field => field === "");
+}
